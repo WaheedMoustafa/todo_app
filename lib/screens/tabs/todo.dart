ee2e4ce9566339ce294c26c33daaf1eee6088020
DonWaHeeD <waheedmoustafa89@gmail.com>
2024-08-24 08:36:14 +0300

    todo initial commit.

--- lib/screens/tabs/todo.dart
+++ lib/screens/tabs/todo.dart
@@ -4,59 +4,93 @@ import '../../models/todo_dm.dart';
 import '../../utils/app_colors.dart';
 import '../../utils/app_styles.dart';
 
-class Todo extends StatelessWidget {
+class Todo extends StatefulWidget {
   final TodoDM item;
 
   const Todo({super.key, required this.item});
 
   @override
+  State<Todo> createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+   bool isDone = false  ;
+
+  @override
+
   Widget build(BuildContext context) {
     return Container(
         height: MediaQuery.of(context).size.height * .13,
         decoration: BoxDecoration(
             color: Colors.white, borderRadius: BorderRadius.circular(22)),
-        margin: EdgeInsets.symmetric(vertical: 22, horizontal: 26),
-        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 20),
+        margin:  const EdgeInsets.symmetric(vertical: 22, horizontal: 26),
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
         child: Row(
           children: [
-            buildVerticalLine(context),
-            SizedBox(
+            buildVerticalLine(context,isDone),
+            const SizedBox(
               width: 25,
             ),
-            buildTodoInfo(),
+            buildTodoInfo(isDone),
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                buildTodoState(),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: isDone == false? AppColors.primary : Colors.white
+                  ),
+                  child: isDone ==false ? Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.primary ,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+                    child:  const Icon(Icons.done, color: Colors.white, size: 15,),
+                  ) :  Container(
+                    color: Colors.white,
+                    child: const Text('Done !',
+                      style: TextStyle(fontWeight: FontWeight.w700,fontSize: 25,color: Colors.green),),
+                  ),
+                onPressed: (){
+                    if(isDone == false){
+                  isDone = !isDone ;
+                setState(() {
+          });
+                    }
+          },
+
+                  //
+    )
               ],
             ),
           ],
         ));
   }
 
-  buildVerticalLine(BuildContext context) => Container(
+  buildVerticalLine(BuildContext context , bool isDone) => Container(
     height: MediaQuery.of(context).size.height * .07,
     width: 4,
     decoration: BoxDecoration(
       borderRadius: BorderRadius.circular(10),
-      color: AppColors.primary,
+      color: isDone == false ? AppColors.primary : Colors.green ,
     ),
   );
 
-  buildTodoInfo() => Expanded(
+  buildTodoInfo( bool isDone) => Expanded(
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Spacer(),
         Text(
-          item.title,
+          widget.item.title,
           maxLines: 1,
           style:
-          AppStyle.bottomSheetTitle.copyWith(color: AppColors.primary),
+          AppStyle.bottomSheetTitle.copyWith(color: isDone == false ? AppColors.primary :Colors.green ),
         ),
         Spacer(),
         Text(
-          item.description,
+          widget.item.description,
           style: AppStyle.bodyTextStyle,
         ),
         Spacer(),
@@ -64,15 +98,22 @@ class Todo extends StatelessWidget {
     ),
   );
 
-  buildTodoState() => Container(
-    decoration: BoxDecoration(
-      color: AppColors.primary,
-      borderRadius: BorderRadius.circular(16),
-    ),
-    padding: EdgeInsets.symmetric(horizontal: 18, vertical: 8),
-    child: Icon(Icons.done, color: Colors.white, size: 15,),
-  );
 
+
+  buildTodoState() => ElevatedButton(
+    onPressed: (){
+      },
+
+    child: Container(
+      decoration: BoxDecoration(
+        color: AppColors.primary ,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+      child: const Icon(Icons.done, color: Colors.white, size: 15,),
+    ),
+  //
+  );
 
 
 }
